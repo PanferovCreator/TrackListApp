24a0ed0b1afc5b2bed4634ed2d5dfdb7bae91ac0
Panferov.creator <panferov.creator@gmail.com>
2023-03-20 08:57:39 +0500

    Initial Commit

---
+++ TrackListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrackListApp
+//
+//  Created by Dmitriy Panferov on 20/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
